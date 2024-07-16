c47f7c9563199075b2c822a2d22918761aab81b3
Piero <mincrami_chae@outlook.com>
2024-07-16 11:32:32 -0500

    first commit

---
+++ src/test/java/pe/arceus/apihub/ApihubApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.arceus.apihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
